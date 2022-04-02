3dee8a35eaa0abc5877717d7f43a787eaf9ac648
Reycel Huffman <reycel.huffman@outlook.com>
2022-04-01 23:00:32 -0500

    almost done

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,30 @@
 // Assignment Code
-// added alert to alert user //
-    alert("Welcome to password generator");
 
-// added a prompt message //
-    let userInput = prompt ("Do you wanna generate a password? type (y)es, (n)o");
-    console.log("user input:"  +userInput);
-
-    let generateBtn = document.querySelector("#generate");
 // write  password generate input //
-    
+let generateBtn = document.querySelector("#generate");
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+let numbers =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let finalPassword = "";
+let userSelection = [""];
+let lengthChoice = "";
+
 // Write password to the #password input
 function writePassword() {
     let password = generatePassword();
     let passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-
 }
 
-// gave generated letters, numbers and symbols for random password //
-    function generatePassword(){
+// Ask user for their input  //
+function generatePassword(){
     let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     let specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-    let numbers =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
+    let numbers =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let finalPassword = "";
-    let passwordItem = "";
     let userSelection = [""];
     
     }
@@ -38,19 +37,19 @@ let passwordSpecialCharacters = window.confirm("Would you like to use special ch
 let passwordNumbers = window.confirm("Would you like to use numbers?");
 
 //if the user don't select the following input then this will be the other options //
-if (passwordLowerCase === false && passwordUpperCase === false && passwordSpecialCharacters && passwordSpecialCharacters === false) {
+if (passwordLowerCase === false && passwordUpperCase === false && passwordSpecialCharacters && passwordNumbers === false) {
     do {
         window.alert("Please select your choice");
         passwordLowerCase = window.confirm("Would you like to use lower case letter?");
         passwordUpperCase = window.confirm("Would you like to use upper case letters?");
         passwordSpecialCharacters = window.confirm("Would you like to use special characters?");
-        passwordNumbers = window.confirm("Would you like to use");
+        passwordNumbers = window.confirm("Would you like to use numbers?");
     }  while (passwordLowerCase === false && passwordUpperCase === false && passwordSpecialCharacters === false && passwordNumbers === false);
  }
 
  // password has to be the length of 8 to 128 characters //
 if (passwordLength < 8 || passwordLength > 128){
-    window.alert("Please enter password length between 8-128");
+    window.alert("Please enter password number of 8 or more and 128 or less. ");
 }
 // User selection input //
 else {
@@ -63,40 +62,19 @@ else {
     if (passwordSpecialCharacters === true){
         userSelection = userSelection.concat(specialCharacters);
     } 
-    if (passwordNumbers === true ) {
+    if (passwordNumbers === true) {
         userSelection = userSelection.concat(numbers);
     }
 }
 // user creates a password using given letter, characters and symbols //
-for (let i = 0; < passwordLength; i ++){
+for (let i = 0; i < passwordLength; i++) {
     let rgn = Math.floor(Math.random() * userSelection.length);
     finalPassword = finalPassword + userSelection[rgn];
     console.log(finalPassword);
-
 }
-return "Generate password will go here!";
 
-    //let randomNumber = Math.floor()*lowerCase.length;
-
-    //let roundedNumber = Math.floor(randomNumber);
-    //console.log(randomNumber);
-    //console.log("randomly generated"  +lowerCase[roundedNumber]);
-
-// ^^ these things are from the actual file along with added video input ^^//
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+return finalPassword;
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-
-
-//function generatePassword(){
-    //console.log("You clicked the button!");
-
-   // 1. Prompt the user for the password criteria
-   // a. Password length 8 < 128
-   // b. lowercase, uppercase, numbers, special characters
-   // 2. Validate the input 
-   // 3. Generate password based on the criteria
-   // 3. Display password to the page. 
+generateBtn.addEventListener("click", writePassword);
 
